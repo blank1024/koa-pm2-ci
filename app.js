8c610312b5ed7f05131d52063d499c13dfd509b9
jawnwa22 <jawnwa22@gmail.com>
2019-11-23 22:51:44 +0800

    update(app.js): no use es6 syntax

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
-import koa from "koa";
-import logger from "koa-logger";
+// import koa from "koa";
+// import logger from "koa-logger";
+const koa = require("koa");
+const logger = require("koa-logger");
 
 const app = new koa();
 const port = 5000;
 
-const hello = ctx => {
+// const hello = ctx => {
+//     ctx.body = "hello";
+// };
+
+const hello = function (ctx) {
     ctx.body = "hello";
-};
+}
 
 app.use(logger());
 app.use(hello);
 
-app.listen(port, () => {
+app.listen(port, function () {
     console.log(`✅  The server is running at http://localhost:${port}/`);
 });
